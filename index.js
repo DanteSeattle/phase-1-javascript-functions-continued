6131b8b8f2577f585403acd522e85a2baba7ced1
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-04-07 12:08:56 -0700

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(string = "roller-skate"){
+    return "This Saturday, I want to " + string +"!";
+}
+const mondayWork = function(string = "go to the office") {
+    return "This Monday, I will " + string + ".";
+}
+function wrapAdjective(flair = "*"){
+    return function(par = "special"){
+        return "You are " + flair + par + flair +"!";
+    }
+}
